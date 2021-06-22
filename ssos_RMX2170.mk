1b110b14da9f98e2a51bd194b293dac8b69ae2a4
Mayur <ultramayur123@gmail.com>
2021-06-22 10:13:17 +0000

    RMX2170: ShapeShift initial bringup
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- ssos_RMX2170.mk
+++ ssos_RMX2170.mk
@@ -7,16 +7,14 @@
 # Inherit from RMX2170 device
 $(call inherit-product, device/realme/RMX2170/device.mk)
 
+# Inherit some common ShapeShift stuff
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
+
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-WITH_GAPPS := true
-
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
-
 # Device identifier
-PRODUCT_NAME := aosp_RMX2170
+PRODUCT_NAME := ssos_RMX2170
 PRODUCT_DEVICE := RMX2170
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 7 Pro
@@ -32,3 +30,12 @@ BUILD_FINGERPRINT := "google/coral/coral:11/RQ3A.210605.005/7349499:user/release
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX2170" \
     TARGET_DEVICE="RMX2170"
+
+# Inherit ShapeShiftOS build stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_FOD_ANIMATIONS := true
+
+# Inherit ShapeShiftOS common properties.
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.ssos.cpu=SD720G
